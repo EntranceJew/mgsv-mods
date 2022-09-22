7ffe809f9e59086ddad06ba7fa41d2926a18deb8
EntranceJew <EntranceJew@gmail.com>
2022-09-22 04:20:51 -0500

    prevent trying to wrangle hashes when there are no new ones

--- _scripts/wrangle_hashes.ps1
+++ _scripts/wrangle_hashes.ps1
@@ -1,5 +1,16 @@
 $config = Get-Content "$PSScriptRoot\..\.config.json" | ConvertFrom-Json
-&"$($config.hashwrangler_path)" "$($config.hashwrangler_target_hashes)" "$($config.strings_path)"
+$target_hash = (Get-FileHash -Path "$($config.hashwrangler_target_hashes)").Hash
+$compare_hash = ""
+if (Test-Path -Path "$($config.hashwrangler_target_hashes).sha256" -PathType Leaf) {
+    $compare_hash = Get-Content -Path "$($config.hashwrangler_target_hashes).sha256"
+}
+if ($target_hash -eq $compare_hash) {
+    "No new hashes."
+} else {
+    &"$($config.hashwrangler_path)" "$($config.hashwrangler_target_hashes)" "$($config.strings_path)"
+    Set-Content -Path "$($config.hashwrangler_target_hashes).sha256" -Value $target_hash
+}
+
 # todo: copy from unknown ih files to target_hashes
 # todo: copy the strings found into a file somewhere idk just push them around so i don't gotta think about it hombre
 # todo: inside InfLookup just make anything we don't know go directly to the hash fondling dungeon i'm tired of pointless blank lookups
